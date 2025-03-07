00398b1ad6e10a009ebba91e355ec029d08200ef
Hieu Le <ngochieu.le1708@gmail.com>
2025-03-07 17:32:36 +0700

    Add proxy Add material UI

--- apps/pet-store/src/app/app.component.ts
+++ apps/pet-store/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClient } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, MatButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pet-store';
+
+  constructor(
+    private _httpClient: HttpClient
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this._httpClient.get(`api/restaurants`).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
