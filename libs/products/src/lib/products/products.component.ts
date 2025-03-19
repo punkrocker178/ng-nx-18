2b63128fe1ed8aeb66e719e710d287285b2e23d0
Hieu Le <ngochieu.le1708@gmail.com>
2025-03-19 23:39:13 +0700

    Implement image list and able to select image

--- libs/products/src/lib/products/products.component.ts
+++ libs/products/src/lib/products/products.component.ts
@@ -1,10 +1,11 @@
-import { Component, effect, input, isDevMode } from '@angular/core';
+import { Component, effect, input, isDevMode, signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../models/api/product.model';
 import { ProductService } from '../services/product.service';
 import { MatButtonModule } from '@angular/material/button';
 import { QueryRequest } from '../models/api/query-request.model';
 import { delay } from 'rxjs';
+import { Image } from '../models/api/image.model';
 
 @Component({
   selector: 'lib-product',
@@ -19,7 +20,9 @@ import { delay } from 'rxjs';
 export class ProductComponent {
   productId = input<string>('');
   product: Product | null = null;
-
+  productImageList: Image[] | undefined;
+  
+  selectedImage: WritableSignal<Image | null> = signal(null);
 
   constructor(
     private _productService: ProductService
@@ -37,7 +40,7 @@ export class ProductComponent {
 
     this._productService.getDetails(this.productId(), queryRequest).pipe(delay(500)).subscribe((product: Product) => {
       this.product = product;
-
+      this.productImageList = product.images;
     });
   }
 
@@ -47,5 +50,15 @@ export class ProductComponent {
 
   public goBack(): void {
     console.log('Navigating back');
+  }
+
+  public selectImage(index: number): void {
+    if (this.productImageList) {
+      this.selectedImage.set(this.productImageList[index]);
+    }
+  }
+
+  public onKeydown(event: KeyboardEvent): void {
+    console.log(event);
   }
 }
